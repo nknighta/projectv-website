4c71498f1600bfb86041d14a9046c761dcddc6d2
NknightA <n.knight.pc0627@gmail.com>
2023-05-22 09:23:17 +0900

    [update] getWidth add.

--- src/component/ResponseMenuTest/container.tsx
+++ src/component/ResponseMenuTest/container.tsx
@@ -1,37 +1,42 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useLayoutEffect} from "react";
 import {Text} from "@chakra-ui/react";
 
 export const getWindowSize = () => {
-    const [windowSize, setWindowSize] = useState({
-        width: 0,
-        height: 0,
-    });
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            const handleResize = () => {
-                setWindowSize({
-                    width:
-                    window.innerWidth,
-                    height: window.innerHeight,
-                });
-            };
-
-            window.addEventListener("resize", handleResize);
-            handleResize();
-            return () => window.removeEventListener("resize", handleResize);
-        } else {
-            return;
-        }
-    }, []);
-    return windowSize;
 };
 
-const DynamicContainer = () => {
-    const {width} = getWindowSize();
-    return (
-        <div>
-            {width}px
-        </div>
-    )
+/**
+ * import React, { useEffect, useRef } from 'react';
+ *
+ * const TabComponent: React.FC = () => {
+ *   const tabRef = useRef<HTMLDivElement>(null);
+ *
+ *   useEffect(() => {
+ *     const handleResize = () => {
+ *       if (tabRef.current) {
+ *         const tabWidth = tabRef.current.offsetWidth;
+ *         console.log('Tab width:', tabWidth, 'px');
+ *       }
+ *     };
+ *
+ *     window.addEventListener('resize', handleResize);
+ *     handleResize(); // 読み込み時にもサイズを計測
+ *
+ *     return () => {
+ *       window.removeEventListener('resize', handleResize);
+ *     };
+ *   }, []);
+ *
+ *   return (
+ *     <div ref={tabRef}>
 }
+*     </div>
+*   )
+;
+*
+}
+;
+*
+*
+export default TabComponent;
+ */
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,18 +11,27 @@ interface Props<T> {
     minWidth: number;
 }
 
-const Links = () =>{
-    const {width} = getWindowSize();
+const HeaderLinkElement = () => {
     return (
-        <>
-
-        </>
+        <Flex w={"30%"} justifyContent={"space-between"} alignItems={"center"}>
+            <Flex alignItems={"center"}>
+                <Link href={"https://github.com/nknighta"} target={"_blank"}>GitHub</Link>
+                <ExternalLinkIcon/>
+            </Flex>
+            <Link href={"/api-list"}>API</Link>
+            <Link href={"/releases"}>releases</Link>
+            <Link href={"/blog"}>Blog</Link>
+            <Link href={"/docs"}>Docs</Link>
+        </Flex>
     );
 };
+
+const {width} = getWindowSize();
 
 function Docs() {
     return (
         <Html>
+            {width}
             <Head></Head>
             <header style={{
                 padding: 7,
@@ -44,16 +53,7 @@ function Docs() {
                             <Text>Web3 API Platform</Text>
                         </Flex>
                     </Box>
-                    <Flex w={"30%"} justifyContent={"space-between"} alignItems={"center"}>
-                        <Flex alignItems={"center"}>
-                            <Link href={"https://github.com/nknighta"} target={"_blank"}>GitHub</Link>
-                            <ExternalLinkIcon/>
-                        </Flex>
-                        <Link href={"/api-list"}>API</Link>
-                        <Link href={"/releases"}>releases</Link>
-                        <Link href={"/blog"}>Blog</Link>
-                        <Link href={"/docs"}>Docs</Link>
-                    </Flex>
+                    <HeaderLinkElement/>
                 </Flex>
             </header>
             <body style={{
